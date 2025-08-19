1da352e834b999a0aabdb4f8c7a2723939e9b734
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-08-19 23:11:45 +0600

    zero hungers assignment

---
+++ zero/zero/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using zero.EF;
+
+
+namespace ZeroHunger.Controllers
+{
+    public class AccountController : Controller
+    {
+        zeroEntities3 db = new zeroEntities3();
+
+        [HttpGet]
+        public ActionResult Login() => View();
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+           
+            var user = (from u in db.Users
+                        where
+                      u.Username.Equals(username) &&
+                      u.Password.Equals(password)
+                        select u).SingleOrDefault();
+            if (user != null)
+            {
+                Session["User"] = user;
+                if (user.Role == "Admin") return RedirectToAction("Index", "Admin");
+                if (user.Role == "Restaurant") return RedirectToAction("Index", "Restaurant");
+                if (user.Role == "Employee") return RedirectToAction("Index", "Employee");
+            }
+
+            TempData["Msg"] = "Username and password is invalid";
+            TempData["Class"] = "danger";
+            return View();
+        }
+
+       
+    }
+}
---
+++ zero/zero/Controllers/RestaurantController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using zero.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        zeroEntities3 db = new zeroEntities3();
+
+        public ActionResult Index()
+        {
+            var user = (User)Session["User"];
+            if (user != null)
+            {
+
+                var requests = (from u in db.Collectrequests
+                                where
+                              u.RestaurantId == user.Id
+                                select u).ToList();
+
+
+                return View(requests);
+            }
+            return View();
+        }
+        [HttpGet ]
+        public ActionResult Create() => View();
+
+        [HttpPost]
+        public ActionResult Create(string foodDetails, DateTime preserveUntil)
+        {
+            var user = (User)Session["User"];
+            if (db.Users.Find(user.Id) == null)
+            {
+                throw new Exception("Restaurant user not found in database!");
+            }
+            else
+            {
+                var req = new Collectrequest
+                {
+                    RestaurantId = user.Id,
+                    Food = foodDetails,
+                    Preserveuntil = preserveUntil,
+                    StatusId = 1,
+                    EmpId = null
+
+                };
+                db.Collectrequests.Add(req);
+              
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
